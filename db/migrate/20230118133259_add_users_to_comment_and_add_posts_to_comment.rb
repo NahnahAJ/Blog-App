4db644145c66416e698527bed2a723ddb829b43d
nahnahAJ <nahnahahjuwah@gmail.com>
2023-01-18 14:09:14 +0000

    Add user_id  and posts_id to comments as a foreign key

---
+++ db/migrate/20230118133259_add_users_to_comment_and_add_posts_to_comment.rb
@@ -0,0 +1,6 @@
+class AddUsersToCommentAndAddPostsToComment < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :comments, :users, null: false, foreign_key: true
+    add_reference :comments, :posts, null: false, foreign_key: true
+  end
+end
