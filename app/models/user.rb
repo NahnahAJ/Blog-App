d9644deb953c609335e74b1dcc75eeaecb29f70a
nahnahAJ <nahnahahjuwah@gmail.com>
2023-01-20 23:49:27 +0000

    Add validation for user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@ class User < ApplicationRecord
   has_many :comments, through: :posts
   has_many :likes, through: :posts
 
+#   Name must not be blank.
+ validates :name, presence: true
+# PostsCounter must be an integer greater than or equal to zero.
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   # A method that returns the 3 most recent posts for a given user.
   def recent_posts
-    Post.where(author: self).order(created_at: :desc).limit(3)
+    Post.where(author_id: self.id).order(created_at: :desc).limit(3)
   end
 end
