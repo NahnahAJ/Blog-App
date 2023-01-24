c24d287085aebe15664ef1510963277e5fd5ab6c
nahnahAJ <nahnahahjuwah@gmail.com>
2023-01-24 14:41:22 +0000

    Define root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+   root "users#index"
   # root "articles#index"
 
   # Defines the route for the users path
