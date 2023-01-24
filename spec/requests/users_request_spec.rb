e0707690c288c985f2db420ee652d934f8e1e90f
nahnahAJ <nahnahahjuwah@gmail.com>
2023-01-24 14:43:32 +0000

    Fix rubocop errors

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /users' do
-    before (:each) {get '/users'}
+    before(:each) { get '/users' }
 
     it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
     it 'returns a list of users' do
-      expect(response.body).to include("Hey there")
+      expect(response.body).to include('Hey there')
     end
 
     # If a correct template was rendered
@@ -19,19 +19,19 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'GET /users/:id' do
-    before (:each) {get '/users/1'}
+    before(:each) { get '/users/1' }
 
     it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
     it 'returns a user' do
-      expect(response.body).to include("Its message from show.html.erb")
+      expect(response.body).to include('Its message from show.html.erb')
     end
 
     # If a correct template was rendered
     it 'renders the show template' do
-      expect(response).to render_template("show")
+      expect(response).to render_template('show')
     end
   end
 end
