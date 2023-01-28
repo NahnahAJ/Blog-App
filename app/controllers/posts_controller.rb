5e553cfefd26e9049a41e4550e1a96b3d01c7eb2
nahnahAJ <nahnahahjuwah@gmail.com>
2023-01-28 03:36:45 +0000

    Add create and new methods to the posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,24 @@ class PostsController < ApplicationController
     @posts = Post.all
     @user = User.where(id: params[:user_id]).first
     @comments = Comment.all
+    @like = Like.all
   end
 
   def show
     @post = Post.find(params[:id])
     @comments = Comment.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    new_post = Post.new(title: params[:post][:title], text: params[:post][:text], author: current_user)
+    if new_post.save
+      redirect_to user_posts_path
+    else
+      render :new
+    end
   end
 end
