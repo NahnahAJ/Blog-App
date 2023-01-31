2a1c5f4d9e8183a1a3cd8612569843920eb53560
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-31 12:55:06 +0100

    Add first test for capybara

--- spec/post_index_spec.rb
+++ spec/post_index_spec.rb
@@ -1,7 +1,8 @@
+
 require_relative 'rails_helper'
 
 RSpec.describe 'Posts Index Page', type: :system do
-  describe 'index page' do
+  describe 'index page(I can see the user\'s profile picture)' do
     before(:each) do
       @user = User.create(name: 'Nahnah', photo: 'https://i.imgur.com/9yG7zZT.jpg')
       @post = Post.create(title: 'Post 1', text: 'This is post 1', author: @user)
@@ -10,5 +11,9 @@ RSpec.describe 'Posts Index Page', type: :system do
       @post4 = Post.create(title: 'Post 4', text: 'This is post 4', author: @user)
       visit user_posts_path(@user)
     end
+
+      it 'should show the profile picture for each user' do
+      expect(page).to have_css("img[src*='https://i.imgur.com/9yG7zZT.jpg']")
+      end
   end
 end
