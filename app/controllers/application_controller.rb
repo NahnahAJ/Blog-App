4dbd7f3ba05b5c5c9c9cda42cbc8314ca2e18f56
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-02 04:20:35 +0000

    Fix all errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
-
   protect_from_forgery with: :exception
-  before_action :authenticate_user! 
+  before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :name, :bio, :photo)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :name, :bio, :photo) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:password, :current_password) }
   end
 end
