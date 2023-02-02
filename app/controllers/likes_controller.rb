7199e7848ddf6c11c2778d61ffcc454b247f6f47
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-02 23:00:17 +0000

    Add destroy method to like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,4 +7,12 @@ class LikesController < ApplicationController
 
     redirect_to user_posts_path
   end
+
+  def destroy
+    like = Like.find(params[:id])
+    like.destroy
+    # reduce the likes counter for the post by 1
+    Post.decrement_counter(:likes_counter, like.post_id)
+    redirect_to user_posts_path
+  end
 end
