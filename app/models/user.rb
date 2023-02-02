3d11f35d9548155621971fb9de9deca7d1001073
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-02 04:15:43 +0000

    Add devise models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :trackable
   has_many :posts
   has_many :comments, through: :posts
   has_many :likes, through: :posts
