5abedf00075778f1a9cc2c50dc391db120c10aa0
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-02 04:14:36 +0000

    Add strong params the lazy method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    # It will return the first user from the database.
-    User.first
+
+  protect_from_forgery with: :exception
+  before_action :authenticate_user! 
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :name, :bio, :photo)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:password, :current_password)}
   end
 end
