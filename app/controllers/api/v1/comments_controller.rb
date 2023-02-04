f39c9df663f1f51ba66b9f505fe6f65c4a72db03
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-04 03:30:53 +0000

    Add create and index actions for comments

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.where(post_id: params[:post_id])
+    render json: @comments
+  end
+
+  def create
+    @comment = Comment.create(comment_params)
+
+    if @comment.save
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+    render json: @comment
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:author_id, :post_id, :text)
+  end
+end
